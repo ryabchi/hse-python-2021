9133e0dd05c5838cfb9f00b394f7b013eefe5b98
Ilya Urtyokov <urtilya@gmail.com>
2021-05-17 10:20:58 +0300

    first com

---
+++ 1/calc.py
@@ -0,0 +1,20 @@
+operator = ['+', '-', '*', '/']
+print("Input operator")
+x = input()
+
+if x not in operator:
+    print("Error. This operator does not exist")
+    exit()
+print("Input two numbers")
+a = float(input())
+b = float(input())
+
+if x == '+':
+    print(f'{a}+{b}={a+b}')
+elif x == '-':
+    print(f'{a}-{b}={a-b}')
+elif x == '*':
+    print(f'{a}*{b}={a*b}')
+else:
+    print(f'{a}/{b}={a/b}')
+
