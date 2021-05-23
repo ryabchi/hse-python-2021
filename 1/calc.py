49bad995d28170560829ad4db7159a570be71bc8
kisozinov <71232712+kisozinov@users.noreply.github.com>
2021-05-23 21:49:02 +0300

    forgot to send it on Monday

---
+++ 1/calc.py
@@ -0,0 +1,20 @@
+operators = ['+','-','*','/','^','mod']
+a = int(input('Enter the first number: '))
+b = int(input('Enter the second number: '))
+op = input('Enter the operation: ')
+if op in operators:
+  if op == '+':
+    res = a + b
+  elif op == '-':
+    res = a - b
+  elif op == '*':
+    res = a * b
+  elif op == '/':
+    res = a / b
+  elif op == '^':
+    res = a ** b
+  elif op == 'mod':
+    res = a % b
+  print('Result is', res)
+else:
+  print("You've entered wrong operation")
