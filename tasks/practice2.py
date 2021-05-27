78b1d39fe89da6451730b2171e7a6f83db48e47f
ZagoskinVA <zagoskin.3@mail.ru>
2021-05-27 14:43:55 +0300

    Done work

--- tasks/practice2.py
+++ tasks/practice2.py
@@ -14,37 +14,25 @@ def generate_payment_message(from_user: str, to_user: str, amount: float) -> str
     Добрый день, Евгений!
     Александр И. перевел вам 100.56 рублей.
     """
-    result = ''
+    name: str = to_user.split(' ')[1]
+    from_user_initial: List[str] = from_user.split(' ')   
+    result = f'Добрый день, {name}!\n{from_user_initial[1]} {from_user_initial[0][0]}. перевел вам {amount:.2f} рублей.'
     # подготовьте данные
     # и используя функции форматирования(например, f-string) отформатируйте строку здесь
     return result
 
 
 def calculate_increased_cashback(operations: List[Tuple]) -> float:
-    """
-    Рассчитывает размер повышенного кешбека для клиента банка.
-    На все покупки - 1%
-    На покупки с повышенным кешбеком - 5%
-
-    На вход принимает:
-    - operations - список кортежей - список операций в формате кортежей в которых 0-вому индексу соответствует сумма
-    платежа, 1-вому индексу - является ли операция - операцией с повышенным кешбеком.
-    Пример:
-    [(500.0, False), (100.0, True)]
-
-    На выходе получаем сумму кешбека на основе текущих операций.
-
-    P.S. Чтобы пробежаться по всем операциям используйте цикл for:
-    for operation in operations:
-        print(operation)  # в переменной operation будет записан кортеж
-
-    """
-    result = 0
+    result: float = 0
+    cashback: dict[bool, lambda:float] = { True: lambda a: a * 0.05, False: lambda a: a * 0.01 }
+    for item in operations:
+        result += cashback[item[1]](item[0])
     # код писать здесь
     return result
 
 
 def clean_user_login(raw_login: str) -> str:
+    login = raw_login.lower().strip().replace('\'', '').replace('"', '')
     """
     Приводит логин пользователя к виду, который используется в нашей системе.
 
@@ -59,12 +47,16 @@ def clean_user_login(raw_login: str) -> str:
     На выходе:
     Строка с очищенным логином.
     """
-    login = None
+
     # код писать здесь
     return login
 
 
 def extract_python_string(raw_string: str) -> str:
+    string: str = raw_string.lower()
+    start_index: int = string.index('python')
+    result = ''
+    result = raw_string[start_index:start_index + 6]
     """
     Функция извлекает слово "python" из строки с сохранением оригинального написания.
 
@@ -75,17 +67,16 @@ def extract_python_string(raw_string: str) -> str:
     Извлекаем слово python в том виде, как оно прописано в исходной строке.
     Например, к строке выше вернем: "PythoN"
     """
-    result = ''
     # код писать здесь
     return result
 
 
 def main() -> None:
-    generate_payment_message(
+    print(generate_payment_message(
         'Шубин Захар Глебович', 'Вишнякова Амалия Станиславовна', 100.555
-    )
-    calculate_increased_cashback([(500.0, False), (100.0, True)])
-    clean_user_login(' a.petrov')
+    ))
+    print(calculate_increased_cashback([(500.0, False), (100.0, True)]))
+    print(clean_user_login(' a.petrov'))
     extract_python_string('Hello, Python!')
 
 
