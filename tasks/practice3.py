29218b29d78a8faba46e4f1d5363100c723b20a0
NANINANINANA <nina@titron.ru>
2021-06-06 00:33:46 +0300

    ptactice3

--- tasks/practice3.py
+++ tasks/practice3.py
@@ -14,8 +14,9 @@ def filter_list(numbers: List[int]) -> List[int]:
     список только из нечетных чисел
     """
 
-    # впишите ваш код здесь
-    return []
+    new_list = [num for num in numbers if num%2 != 0]
+
+    return new_list
 
 
 def get_popular_category(operations: List[Dict[str, Any]]) -> str:
@@ -30,8 +31,13 @@ def get_popular_category(operations: List[Dict[str, Any]]) -> str:
     строка - название категории в которой клиент совершил наибольшее количество покупок.
     """
 
-    # впишите ваш код здесь
-    return ''
+    max_amount = -1
+    for oper in operations:
+        if oper['amount'] > max_amount:
+            max_amount = oper['amount']
+            max_category = oper['category']
+
+    return max_category
 
 
 def hide_personal_info(info: Dict[str, Any]) -> Dict[str, Any]:
@@ -48,7 +54,11 @@ def hide_personal_info(info: Dict[str, Any]) -> Dict[str, Any]:
     - словарь в котором все персональные данные из описания функции - скрыты по алгоритму выше.
     """
 
-    # впишите ваш код здесь
+    if 'passport_code' in info.keys():
+        info['passport_code'] = ''.join(['*' if sym.isdigit() else sym for sym in info['passport_code']])
+    if 'phone_number' in info.keys():
+        info['phone_number'] = ''.join(['*' if sym.isdigit() else sym for sym in info['phone_number']])
+
     return info
 
 
