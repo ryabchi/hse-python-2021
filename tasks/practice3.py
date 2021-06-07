3f9e4df0b87eb088eb3fda67f60813f308cbb6d4
Irina <pasmanikirina29@gmail.com>
2021-06-07 15:29:10 +0300

    p3     ex 2 and 3 fixes

--- tasks/practice3.py
+++ tasks/practice3.py
@@ -10,32 +10,34 @@ def filter_list(numbers: List[int]) -> List[int]:
 
 
 def get_popular_category(operations: List[Dict[str, Any]]) -> str:
-    for cat in operations:
-        if max_cat < cat['amount']:
-            max_cat = cat
+    cats = dict()
+    max_cat_amount = 0
+    max_cat = ''
+    for operation in operations:
+        for cat in cats:
+            if operation['category'] == cat['category']:
+                cats['amount'] += operation['amount']
+            else:
+                cats[operation['category']] = operation['amount']
+    for cat in cats:
+        if max_cat_amount < cat['amount']:
+            max_cat = cat['category']
     return max_cat
 
 
 def hide_personal_info(info: Dict[str, Any]) -> Dict[str, Any]:
-    """
-    Функция принимает на вход словарь содержащий информацию о клиенте.
-    В словаре могут быть персональные данные клиента: ключи passport_code и phone_number.
-    Если поля присутствуют - нужно защитить эти данные от злоумышленников.
-    Для этого заменим все цифры в значениях этих полей на символ *.
-
-    На вход:
-    - info - словарь содержащий персональные данные клиента
-
-    На выходе:
-    - словарь в котором все персональные данные из описания функции - скрыты по алгоритму выше.
-    """
+    new_str = ''
     for client in info:
-        if client['passport_code'] in info:
-            for num in client['passport_code']:
-                num = '*'
-        if client['phone_number'] in info:
-            for num in client['phone_number']:
-                num = '*'
+        if 'passport_code' in client:
+            tmp = len(str(client[passport_code]))
+            for i in range(tmp):
+                new_str += '*'
+            client[passport_code] = new_str
+        if 'phone_number' in client:
+            tmp = len(str(client[phone_number]))
+            for i in range(tmp):
+                new_str += '*'
+            client[phone_number] = new_str
     return info
 
 
