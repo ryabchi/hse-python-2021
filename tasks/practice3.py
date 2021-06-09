971a85f2ea73f9a1f66ba7a10aa7242f2f5cb1d9
nastyarogozyan <rogozyan.01@mail.ru>
2021-06-09 15:24:45 +0300

    practice3

--- tasks/practice3.py
+++ tasks/practice3.py
@@ -1,20 +1,17 @@
 from typing import List, Dict, Any
-
-
 def filter_list(numbers: List[int]) -> List[int]:
     """
     Используя генератор списков (list comprehension) напишите код,
     который отфильтровывает все четные числа в списке.
     И возвращает результат фильтрации.
-
     На вход:
     - numbers - список состоящий из чисел
-
     На выходе:
     список только из нечетных чисел
     """
 
     # впишите ваш код здесь
+    return []
     return [x for x in numbers if x % 2 != 0]
 
 
@@ -22,21 +19,19 @@ def get_popular_category(operations: List[Dict[str, Any]]) -> str:
     """
     Функция анализирует список трат клиента по различным категориям товаров
     и находит категорию, в которой человек совершил больше всего покупок.
-
     На вход:
     - operations - список словарей в формате [{'category': 'название категории', 'amount': 100}]
-
     На выходе:
     строка - название категории в которой клиент совершил наибольшее количество покупок.
     """
 
     # впишите ваш код здесь
     return ''
-    set_operat = set(r['category'] for r in operations)
-    cleaned_operat = {r: 0 for r in set_operat}
-    for r in operations:
-        cleaned_operat[r['category']] += r['amount']
-    return max(cleaned_operat, key=lambda x: cleaned_operat[x])
+    list_categories = {category['category'] for category in operations}
+    amount_category = {amount: 0 for amount in list_categories}
+    for category in operations:
+        amount_category[category['category']] += category['amount']
+    return max(amount_category, key=lambda x: amount_category[x])
 
 
 def hide_personal_info(info: Dict[str, Any]) -> Dict[str, Any]:
@@ -45,27 +40,30 @@ def hide_personal_info(info: Dict[str, Any]) -> Dict[str, Any]:
     В словаре могут быть персональные данные клиента: ключи passport_code и phone_number.
     Если поля присутствуют - нужно защитить эти данные от злоумышленников.
     Для этого заменим все цифры в значениях этих полей на символ *.
-
     На вход:
     - info - словарь содержащий персональные данные клиента
-
     На выходе:
     - словарь в котором все персональные данные из описания функции - скрыты по алгоритму выше.
     """
 
     # впишите ваш код здесь
-    symbols_to_encrypt = ['1','2','3','4','5','6','7','8','9','0']
-    key_list = info.keys()
+    symbols_to_encrypt = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     category_to_encrypt = ['passport_code', 'phone_number']
-    for i in category_to_encrypt:
-        if i in key_list:
-            info[i] = ''.join('*' if j in symbols_to_encrypt else j for j in info[i])
+    keys = info.keys()
+    for key in keys:
+        for cat in category_to_encrypt:
+            if key == cat:
+                secure = ""
+                for symbol in info[cat]:
+                    if symbol in symbols_to_encrypt:
+                        secure += '*'
+                    else:
+                        secure += symbol
+                info[key] = secure
     return info
 
 
 def main() -> None:
     pass
-
-
 if __name__ == '__main__':
     main()
