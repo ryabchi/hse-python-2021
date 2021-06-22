4b57232edc0db4ab3529cb4c4871a19ff51eee20
Shaidi <drsharibzhanova@edu.hse.ru>
2021-06-22 12:54:14 +0300

    lab2

---
+++ tasks/practice3.py
@@ -0,0 +1,78 @@
+from typing import List, Dict, Any
+
+
+def filter_list(numbers: List[int]) -> List[int]:
+    """
+    Используя генератор списков (list comprehension) напишите код,
+    который отфильтровывает все четные числа в списке.
+    И возвращает результат фильтрации.
+    На вход:
+    - numbers - список состоящий из чисел
+    На выходе:
+    список только из нечетных чисел
+    """
+
+    # впишите ваш код здесь
+    return [i for i in numbers if i % 2]
+
+
+def get_popular_category(operations: List[Dict[str, Any]]) -> str:
+    """
+    Функция анализирует список трат клиента по различным категориям товаров
+    и находит категорию, в которой человек совершил больше всего покупок.
+    На вход:
+    - operations - список словарей в формате [{'category': 'название категории', 'amount': 100}]
+    На выходе:
+    строка - название категории в которой клиент совершил наибольшее количество покупок.
+    """
+
+    # впишите ваш код здесь
+    global minimal_category
+    minimal_amount = -1
+    for operation in operations:
+         if operation['amount'] > minimal_amount:
+             minimal_amount = operation['amount']
+             minimal_category = operation['category']
+    return minimal_category
+
+
+def hide_personal_info(info: Dict[str, Any]) -> Dict[str, Any]:
+    """
+    Функция принимает на вход словарь содержащий информацию о клиенте.
+    В словаре могут быть персональные данные клиента: ключи passport_code и phone_number.
+    Если поля присутствуют - нужно защитить эти данные от злоумышленников.
+    Для этого заменим все цифры в значениях этих полей на символ *.
+    На вход:
+    - info - словарь содержащий персональные данные клиента
+    На выходе:
+    - словарь в котором все персональные данные из описания функции - скрыты по алгоритму выше.
+    """
+
+    # впишите ваш код здесь
+    def secure(_temp : str) -> str:
+        if i.isnumeric():
+            _temp += '*'
+        else:
+            _temp += i
+        return _temp
+
+    temp = ""
+    if info.get("passport_code", 0):
+        for i in info["passport_code"]:
+            temp = secure(temp)
+        info["passport_code"] = temp
+
+    temp = ""
+    if info.get("phone_number", 0):
+        for i in info["phone_number"]:
+            temp = secure(temp)
+        info["phone_number"] = temp
+    return info
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == '__main__':
+    main()
