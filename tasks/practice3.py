c7004b9b1376c416976c825cea14ea0a8e29ab59
slavokeru <70898932+slavokeru@users.noreply.github.com>
2021-06-23 22:02:15 +0300

    Add files via upload

--- tasks/practice3.py
+++ tasks/practice3.py
@@ -1,60 +1,29 @@
 from typing import List, Dict, Any
 
+import re
 
 def filter_list(numbers: List[int]) -> List[int]:
-    """
-    Используя генератор списков (list comprehension) напишите код,
-    который отфильтровывает все четные числа в списке.
-    И возвращает результат фильтрации.
 
-    На вход:
-    - numbers - список состоящий из чисел
-
-    На выходе:
-    список только из нечетных чисел
-    """
-
-    # впишите ваш код здесь
-    return []
+    return [i for i in numbers if i % 2 != 0]
 
 
 def get_popular_category(operations: List[Dict[str, Any]]) -> str:
-    """
-    Функция анализирует список трат клиента по различным категориям товаров
-    и находит категорию, в которой человек совершил больше всего покупок.
 
-    На вход:
-    - operations - список словарей в формате [{'category': 'название категории', 'amount': 100}]
 
-    На выходе:
-    строка - название категории в которой клиент совершил наибольшее количество покупок.
-    """
+    caty = {i:0 for i in [j['category'] for j in operations]}
 
-    # впишите ваш код здесь
-    return ''
+    for i in operations:
+        caty[i['category']] += i['amount']
 
+    return max(caty, key=lambda x: caty[x])
 
 def hide_personal_info(info: Dict[str, Any]) -> Dict[str, Any]:
-    """
-    Функция принимает на вход словарь содержащий информацию о клиенте.
-    В словаре могут быть персональные данные клиента: ключи passport_code и phone_number.
-    Если поля присутствуют - нужно защитить эти данные от злоумышленников.
-    Для этого заменим все цифры в значениях этих полей на символ *.
+ 
 
-    На вход:
-    - info - словарь содержащий персональные данные клиента
-
-    На выходе:
-    - словарь в котором все персональные данные из описания функции - скрыты по алгоритму выше.
-    """
-
-    # впишите ваш код здесь
-    return info
-
+    return {key: re.sub(r'[0-9]', "*", value) for key, value in info.items()}
 
 def main() -> None:
-    pass
-
+    print(get_popular_category([{'category': 'супермаркеты', 'amount': 3380}, {'category': 'рестораны', 'amount': 5649}]))
 
 if __name__ == '__main__':
     main()
